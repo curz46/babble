cffb31ffa72bccbb16a5825e4602dbd224f8e79b
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2019-10-24 10:45:00 +0100

    Replace tabs with spaces

--- src/babble.c
+++ src/babble.c
@@ -10,80 +10,80 @@
 #include "gateway.h"
 
 #define MAX_SEGMENTS 100 // Dynamic allocation too hard, just make it
-						 // "large enough"
+                         // "large enough"
 
 char* concat(const char* str1, const char* str2) {
-	// Allow 1 byte for the NULL terminator
-	char* result = malloc(1 + strlen(str1) + strlen(str2));
-	if (result != NULL) {
-		result[0] = '\0';
-		strcat(result, str1);
-		strcat(result, str2);
-		return result;
-	} else {
-		return NULL;
-	}
+    // Allow 1 byte for the NULL terminator
+    char* result = malloc(1 + strlen(str1) + strlen(str2));
+    if (result != NULL) {
+        result[0] = '\0';
+        strcat(result, str1);
+        strcat(result, str2);
+        return result;
+    } else {
+        return NULL;
+    }
 }
 
 size_t on_receive(char* data, size_t size, size_t nmemb, char** content) {
-	char* result;
-	result = concat(*content, data);
-	if (result == NULL) {
-		fprintf(stderr, "Couldn't concat data to content.");
-		exit(1);
-	}
-	*content = result;
-	
-	// libcurl expects this
-	return size * nmemb;
+    char* result;
+    result = concat(*content, data);
+    if (result == NULL) {
+        fprintf(stderr, "Couldn't concat data to content.");
+        exit(1);
+    }
+    *content = result;
+    
+    // libcurl expects this
+    return size * nmemb;
 }
 
 int main() {
-	curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl_global_init(CURL_GLOBAL_DEFAULT);
 
-	const CURL* curl = curl_easy_init();
-	if (!curl) {
-		printf("Failed to initialised curl!");
-		exit(1);
-	}
+    const CURL* curl = curl_easy_init();
+    if (!curl) {
+        printf("Failed to initialised curl!");
+        exit(1);
+    }
 
-	char* data = "";
+    char* data = "";
 
-	curl_easy_setopt(curl, CURLOPT_URL, BASE GATEWAY_GET);
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, on_receive);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
+    curl_easy_setopt(curl, CURLOPT_URL, BASE GATEWAY_GET);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, on_receive);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
 
-	const CURLcode result = curl_easy_perform(curl);
+    const CURLcode result = curl_easy_perform(curl);
 
-	if (result == CURLE_OK) {
-		long response_code = 0;
-		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-		printf("Response Code: %i\n", response_code);
-	} else {
-		printf("Error: %s\n", curl_easy_strerror(result));
-	}
+    if (result == CURLE_OK) {
+        long response_code = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+        printf("Response Code: %i\n", response_code);
+    } else {
+        printf("Error: %s\n", curl_easy_strerror(result));
+    }
 
-	printf("Final content: %s\n", data);
+    printf("Final content: %s\n", data);
 
-	curl_easy_cleanup(curl);
-	curl_global_cleanup();
-	
-	const nx_json* json = nx_json_parse(data, 0);
-	if (!json) {
-		fprintf(stderr, "Failed to parse JSON from GATEWAY_GET response");
-		exit(1);
-	}
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    
+    const nx_json* json = nx_json_parse(data, 0);
+    if (!json) {
+        fprintf(stderr, "Failed to parse JSON from GATEWAY_GET response");
+        exit(1);
+    }
 
-	const char* url = nx_json_get(json, "url")->text_value;
-	printf("URL: %s\n", url);
+    const char* url = nx_json_get(json, "url")->text_value;
+    printf("URL: %s\n", url);
 
-	printf("Attempting WS connection...\n");
-	struct uwsc_client* client = gateway_open_connection(url);
-	// Blocking loop
-	gateway_init_loop(client);
-	free(client);
+    printf("Attempting WS connection...\n");
+    struct uwsc_client* client = gateway_open_connection(url);
+    // Blocking loop
+    gateway_init_loop(client);
+    free(client);
 
-	printf("Exiting...\n");
+    printf("Exiting...\n");
 
-	return 0;
+    return 0;
 }
--- src/gateway.c
+++ src/gateway.c
@@ -6,43 +6,43 @@
 #define EV_LOOP EV_DEFAULT
 
 struct uwsc_client* gateway_open_connection(char* url) {
-  	const int PING_INTERVAL = 10; // seconds
-	struct uwsc_client *client;
-	client = uwsc_new(EV_LOOP, url, PING_INTERVAL, NULL);
+    const int PING_INTERVAL = 10; // seconds
+    struct uwsc_client *client;
+    client = uwsc_new(EV_LOOP, url, PING_INTERVAL, NULL);
 
-	if (!client) {
-		fprintf(stderr, "Failed to establish gateway connection. Exiting...\n");
-		exit(1);
-	}
+    if (!client) {
+        fprintf(stderr, "Failed to establish gateway connection. Exiting...\n");
+        exit(1);
+    }
 
-	return client;
+    return client;
 }
 
 void gateway_onopen(struct uwsc_client* client) {
-	printf("onopen\n");
+    printf("onopen\n");
 }
 
 void gateway_onmessage(struct uwsc_client* client,
-					   void   *data,
-					   size_t length,
-					   bool   binary) {
-	printf("onmessage:\n");
-	printf("binary: %i\n", binary);
-	if (binary) {
-		// ...
-	} else {
-		printf("string: %s\n", data);
-	}
-}	
+                       void   *data,
+                       size_t length,
+                       bool   binary) {
+    printf("onmessage:\n");
+    printf("binary: %i\n", binary);
+    if (binary) {
+        // ...
+    } else {
+        printf("string: %s\n", data);
+    }
+}   
 
 void gateway_init_loop(struct uwsc_client* client) {
-	client->onopen    = gateway_onopen;
-	client->onmessage = gateway_onmessage;
+    client->onopen    = gateway_onopen;
+    client->onmessage = gateway_onmessage;
 
-	// ??
-	struct ev_signal signal_watcher;
-	ev_signal_init(&signal_watcher, NULL, SIGINT);
-	ev_signal_start(EV_LOOP, &signal_watcher);
+    // ??
+    struct ev_signal signal_watcher;
+    ev_signal_init(&signal_watcher, NULL, SIGINT);
+    ev_signal_start(EV_LOOP, &signal_watcher);
 
-	ev_run(EV_LOOP, 0);
+    ev_run(EV_LOOP, 0);
 }
